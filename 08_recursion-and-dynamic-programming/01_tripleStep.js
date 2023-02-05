ba16f0d12e3c5f3784564f8263a9f319b2b442be
Sofya Tuymedova <stuymedova@gmail.com>
2023-02-05 22:32:21 +0300

    Add tripleStep

---
+++ 08_recursion-and-dynamic-programming/01_tripleStep.js
@@ -0,0 +1,28 @@
+/**
+ * A child is running up a staircase with n steps and can 
+ * hop either 1 step, 2 steps, or 3 steps at a time. 
+ * Implement a method to count how many possible ways the 
+ * child can run up the stairs.
+ */
+
+function tripleStep(numberOfSteps, memo = []) {
+  if (numberOfSteps < 0) {
+    return 0;
+  }
+  if (numberOfSteps === 0) {
+    return 1;
+  }
+  if (!memo[numberOfSteps]) {
+    memo[numberOfSteps] = 
+      tripleStep(numberOfSteps - 1, memo) + 
+      tripleStep(numberOfSteps - 2, memo) + 
+      tripleStep(numberOfSteps - 3, memo)
+  }
+  return memo[numberOfSteps];
+}
+
+// Test cases:
+console.log(tripleStep(1)); // 1
+console.log(tripleStep(2)); // 2
+console.log(tripleStep(3)); // 4
+console.log(tripleStep(4)); // 7
