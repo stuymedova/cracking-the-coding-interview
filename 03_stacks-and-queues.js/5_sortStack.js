85e57fb83fe0672582f425a83f6f42b8bbeab2e6
Sofya Tuymedova <stuymedova@gmail.com>
2023-07-18 10:27:24 +0300

    Add second implementation of sortStack

--- 03_stacks-and-queues.js/5_sortStack.js
+++ 03_stacks-and-queues.js/5_sortStack.js
@@ -9,7 +9,10 @@
 
 import { Stack } from '../data-structures/Stack.js'
 
-function sortStack(givenStack) {
+// Implementation 1: two additional stacks.
+function sortStack1(givenStack) {
+	// Sorted stack will contain elements in the reverse
+	// order.
 	const sortedStack = new Stack();
 	const temporaryStack = new Stack();
 
@@ -37,6 +40,29 @@ function sortStack(givenStack) {
 	return givenStack;
 }
 
+// Implementation 2: one additional stack.
+function sortStack2(givenStack) {
+	// Sorted stack will contain elements in the reverse
+	// order.
+	const sortedStack = new Stack();
+
+	while (!givenStack.isEmpty()) {
+		const value = givenStack.pop();
+
+		while (!sortedStack.isEmpty() && value > sortedStack.peek()) {
+			givenStack.push(sortedStack.pop());
+		}
+
+		sortedStack.push(value);
+	}
+
+	while (!sortedStack.isEmpty()) {
+		givenStack.push(sortedStack.pop());
+	}
+
+	return givenStack;
+}
+
 // TEST CASES
 const stack = new Stack()
 	.push(3)
@@ -48,8 +74,17 @@ const stack2 = new Stack()
 	.push(1)
 	.push(2);
 
-const sortedStack = sortStack(stack);
-console.log(sortedStack.stack);
+const stack3 = new Stack()
+	.push(3)
+	.push(2)
+	.push(1)
+	.push(2);
 
-const sortedStack2 = sortStack(stack2);
-console.log(sortedStack2.stack);
+const sortedStack = sortStack2(stack);
+console.log(sortedStack.stack); // [1, 2, 3]
+
+const sortedStack2 = sortStack2(stack2);
+console.log(sortedStack2.stack); // [1, 2, 3]
+
+const sortedStack3 = sortStack2(stack3);
+console.log(sortedStack3.stack); // [1, 2, 2, 3]
